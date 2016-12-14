16349b7ac078182b3de4bcf42e2bb78d095aec80
Kevin Burton <rkevinburton@charter.net>
2016-12-14 13:30:18 -0600

    Use the created barrel

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,8 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { CustomerComponent } from './customer/customer.component';
-import { CustomersComponent } from './customer/customers.component';
+import { CustomerComponent, CustomersComponent } from './customer';
 
 @NgModule({
   declarations: [
